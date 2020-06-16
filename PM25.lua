e50efa77d441bbf700a8669973bbdc8327f8aeb1
njjjay <njjjay0927@gmail.com>
2020-06-16 10:03:15 +0800

    每小时200次使用限制 建议自己申请api
    9月api到期

---
+++ PM25.lua
@@ -0,0 +1,60 @@
+﻿local log = require("log")
+local Api = require("coreApi")
+local json = require("json")
+local http = require("http")
+function ReceiveFriendMsg(CurrentQQ, data)
+    return 1
+end
+function ReceiveGroupMsg(CurrentQQ, data)
+	if data.FromUserId ==2986807981 then--防止自我复读
+		  return 1 end
+
+if string.find(data.Content, "PM2.5") then 
+	keyWord = data.Content:gsub("PM2.5", "")
+	if keyWord=="" then keyWord="上海" end --默认上海
+	querystr="app=weather.pm25&weaid="..keyWord.."&appkey=52181&sign=b6f18ac4d414ea7d1d31f395efeafd34"
+   -- log.notice("querystr=\n%s", querystr)--	
+        response, error_message =
+            http.request(
+            "POST",
+            "http://api.k780.com" ,--从nowapi接口获取
+		{query=""..querystr}
+        )
+        local html = response.body
+
+	resultstr = json.decode(html) --反序列化json
+  --log.notice("html=\n%s", html)
+  --log.notice("resultstr=\n%s", resultstr)
+		ApiRet =				
+            Api.Api_SendMsg( 
+            CurrentQQ,
+            {
+                toUser = data.FromGroupId,
+                sendToType = 2,
+                sendMsgType = "TextMsg",
+                content = keyWord.."aqi:".. resultstr.result.aqi.."\n空气质量:" .. resultstr.result.aqi_levnm, --获取最后发布时间 ,
+		groupid = 0,
+
+                atUser = 0,
+
+            }
+        )
+	html = nil
+
+    end	
+
+    return 1
+end
+function ReceiveEvents(CurrentQQ, data, extData)
+    return 1
+end
+
+function cutstring(str)
+po1="<div class=\"writing\"><p>"
+po2="</p><p></p><p><div style"
+_,i=string.find(str, po1)
+j,_=string.find(str, po2)
+
+result=string.sub(str,i+1,j-1) --去除头尾
+return result
+end
