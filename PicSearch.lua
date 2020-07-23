fd163c3f369158600019da861eafe07de6cc88f0
njjjay <njjjay0927@gmail.com>
2020-07-23 10:38:09 +0800

    改良了搜图插件
    解决了返回的json没有ext_url时不会发送消息的问题

---
+++ PicSearch.lua
@@ -0,0 +1,176 @@
+local log = require("log")
+local Api = require("coreApi")
+local json = require("json")
+local http = require("http")
+
+function ReceiveFriendMsg(CurrentQQ, data)
+
+if data.FromUin ==2986807981 then--防止自我复读
+		  return 1 end
+  		if string.find(data.MsgType, "PicMsg") then 
+				str = json.decode(data.Content)
+				log.notice("str.Content--->  %s", str.Content)
+				if str.Content == nil then
+					return 1
+				end
+				if string.find(str.Content, "搜图") then
+				loadingF(CurrentQQ,data)
+  				img_url = str.FriendPic[1].Url--私聊搜图是FriendPic
+				log.notice("MsgType--->   %s", data.MsgType)
+  				log.notice("img_url--->   %s", img_url)
+          response, error_message =
+                 http.request(
+                 "GET",
+          				"https://saucenao.com/search.php?",
+                 {
+                     query = "db=999&output_type=2&testmode=1&numres=1&url=" ..
+                         img_url,
+                     headers = {
+          								["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
+					}
+                 }
+             )
+  				local html = response.body
+  				-- log.notice("html-->%s",html)
+  				local re = json.decode(html)
+  				-- log.notice("re---> %s", re)
+  				local similarity = re.results[1].header.similarity
+  				local thumbnail_url = re.results[1].header.thumbnail
+  				local title = re.results[1].data.title
+  				local pixiv_id = re.results[1].data.pixiv_id
+  				local member_name = re.results[1].data.member_name
+				local data_source = re.results[1].data.source
+				local data_creator =  re.results[1].data.creator[1]
+				local ext_urls = ""
+				if string.find(html, "ext_urls") then --api返回的结果可能没有ext_url字段
+					ext_urls = re.results[1].data.ext_urls[1]
+					end
+          luaRes =
+              Api.Api_SendMsg(--调用发消息的接口
+              CurrentQQ, 
+			 { 
+		        toUser = data.FromUin, --回复当前消息的来源群ID
+		        sendToType = 1, --2发送给群1发送给好友3私聊
+		        sendMsgType = "PicMsg", --进行文本复读回复 
+		        content = string.format(
+  									"\n相似度：%s\n标题：%s\nPixiv_ID：%d\n插画家昵称：%s\n插画链接：%s\n",
+  									similarity,
+  									title,
+  									pixiv_id,
+  									member_name,
+									ext_urls
+  								).."其他信息:"..data_source.."\n"..
+								data_creator, --回复内容
+				picUrl = thumbnail_url,
+  				picBase64Buf = "",
+  				fileMd5 = "" 
+		    }
+          )
+          -- log.notice("From Lua SendMsg Ret-->%d", luaRes.Ret)
+      end
+	end
+
+
+
+    return 1
+end
+function ReceiveGroupMsg(CurrentQQ, data)
+	if data.FromUserId ==2986807981 then--防止自我复读
+		  return 1 end
+  		if string.find(data.MsgType, "PicMsg") then 
+				str = json.decode(data.Content)
+				log.notice("str.Content--->  %s", str.Content)
+				if str.Content == nil then
+					return 1
+				end
+				if string.find(str.Content, "搜图") then
+					loadingG(CurrentQQ,data)
+  				img_url = str.GroupPic[1].Url
+          log.notice("MsgType--->   %s", data.MsgType)
+  				log.notice("img_url--->   %s", img_url)
+          response, error_message =
+                 http.request(
+                 "GET",
+          				"https://saucenao.com/search.php?",
+                 {
+                     query = "db=999&output_type=2&testmode=1&numres=1&url=" ..
+                         img_url,
+                     headers = {
+          								["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
+                     }
+                 }
+             )
+  				local html = response.body
+  				-- log.notice("html-->%s",html)
+  				local re = json.decode(html)
+  				-- log.notice("re---> %s", re)
+  				local similarity = re.results[1].header.similarity
+  				local thumbnail_url = re.results[1].header.thumbnail
+  				local title = re.results[1].data.title
+  				local pixiv_id = re.results[1].data.pixiv_id
+  				local member_name = re.results[1].data.member_name
+				local data_source = re.results[1].data.source
+				local data_creator =  re.results[1].data.creator[1]
+				local ext_urls = ""
+				if string.find(html, "ext_urls") then --api返回的结果可能没有ext_url字段
+					ext_urls = re.results[1].data.ext_urls[1]
+					end
+          luaRes =
+              Api.Api_SendMsg(--调用发消息的接口
+              CurrentQQ,
+              {
+                  toUser = data.FromGroupId, --回复当前消息的来源群ID
+                  sendToType = 2, --2发送给群1发送给好友3私聊
+                  sendMsgType = "PicMsg", --进行文本复读回复
+  								content = string.format(
+  									"\n相似度：%s\n标题：%s\nPixiv_ID：%d\n插画家昵称：%s\n插画链接：%s\n",
+  									similarity,
+  									title,
+  									pixiv_id,
+  									member_name,
+									ext_urls
+  								).."其他信息:"..data_source.."\n"..
+								data_creator,
+  								picUrl = thumbnail_url,
+  								picBase64Buf = "",
+  								fileMd5 = ""
+              }
+          )
+          -- log.notice("From Lua SendMsg Ret-->%d", luaRes.Ret)
+      end
+		end
+      return 1
+  end
+function ReceiveEvents(CurrentQQ, data, extData)
+    return 1
+end
+function loadingG(CurrentQQ,data)
+		luaMsg =
+		    Api.Api_SendMsg(--调用发消息的接口
+		    CurrentQQ,
+		    {
+		        toUser = data.FromGroupId, --回复当前消息的来源群ID
+		        sendToType = 2, --2发送给群1发送给好友3私聊
+		        sendMsgType = "TextMsg", --进行文本复读回复
+		        groupid = 0, --不是私聊自然就为0咯
+		        content = "正在查询saucenao和danboru(未上传p站大概率搜不到)[表情67]", --回复内容
+		        atUser = 0 --是否 填上data.FromUserId就可以复读给他并@了
+		    }
+		)
+	end
+function loadingF(CurrentQQ,data)
+		luaMsg =
+		    Api.Api_SendMsg(--调用发消息的接口
+		    CurrentQQ,
+		    {
+		        toUser = data.FromUin, --回复当前消息的来源群ID
+		        sendToType = 1, --2发送给群1发送给好友3私聊
+		        sendMsgType = "TextMsg", --进行文本复读回复
+		        groupid = 0, --不是私聊自然就为0咯
+		        content = "正在查询saucenao和danboru(未上传p站大概率搜不到)[表情67]", --回复内容
+		        atUser = 0 --是否 填上data.FromUserId就可以复读给他并@了
+		    }
+		)
+	end
+	
+	
